e63de35cb057c5055267450987626e13d41e361e
Ja1Ribeiro <ja1pribeiros@gmail.com>
2024-11-15 03:43:41 -0300

    Adição dos cenarios de Emprestimo para PF e Consorcio para PJ

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,3 +9,15 @@ Cypress.Commands.add('visitCalculator', (username, password) => {
   cy.loginInit(username, password);
   cy.get('.MuiList-root > :nth-child(2) > .MuiButtonBase-root').click();
 });
+
+Cypress.Commands.add('fillFirstInformations', (birth, property_value, financing_value, term) => {
+  cy.get('#\\:rn\\:').type(birth);
+  cy.get('#\\:ro\\:').type(property_value);
+  cy.get('#\\:rp\\:').type(financing_value);
+  cy.get('#\\:rs\\:').type(term);
+});
+
+Cypress.Commands.add('readAndAccept', () => {
+  cy.get('.PrivateSwitchBase-input').click();
+  cy.get('button[type="submit"]').click();
+});
